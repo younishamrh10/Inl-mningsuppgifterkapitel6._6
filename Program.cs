6cca81969a533424b64d1f1f6fce4dcb8232d9a5
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-02-28 10:04:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uppgift_6_6
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en text");
+            string text = Console.ReadLine().ToLower();
+            string[] ord = text.Split(' ');
+            Förändring(ord);
+            Console.ReadKey();
+        }
+        static void Förändring(string[] ord)
+        {
+            for (int i = 0; i < ord.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(ord[i].ToUpper() + " ");
+                }
+                else
+                {
+                    Console.Write(ord[i].ToLower() + " ");
+                }
+            }
+        }
+    }
+}
